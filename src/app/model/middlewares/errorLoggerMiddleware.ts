a51edd91addefaf005b1d7ebf239d10987848573
NickPaferov <paferov.nikolai@gmail.com>
2023-11-20 20:14:08 +0300

    implement error demonstration depending on statusCode

--- src/app/model/middlewares/errorLoggerMiddleware.ts
+++ src/app/model/middlewares/errorLoggerMiddleware.ts
@@ -4,10 +4,22 @@ import type { Middleware } from '@reduxjs/toolkit'
 
 export const errorLoggerMiddleware: Middleware = () => (next) => (action) => {
   if (isRejectedWithValue(action)) {
-    console.warn(
-      action.payload?.data?.message?.message ||
-        'Произошла ошибка. Повторите попытку.',
-    )
+    console.warn(action)
+    const statusCode = action.payload?.status
+    switch (statusCode) {
+      case 401: {
+        alert('Ошибка авторизации. Проверьте логин и пароль')
+        break
+      }
+      case 'FETCH_ERROR': {
+        alert('Не проходит запрос на сервер')
+        break
+      }
+      default: {
+        alert('Произошла ошибка. Попробуйте ещё раз')
+        break
+      }
+    }
   }
   return next(action)
 }

4287ba4cffc7314ebe9c74efac94e6a2363d5e2f
NickPaferov <paferov.nikolai@gmail.com>
2023-11-26 13:45:42 +0300

    remove unused element in errorLoggerMiddleware

--- src/app/model/middlewares/errorLoggerMiddleware.ts
+++ src/app/model/middlewares/errorLoggerMiddleware.ts
@@ -1,26 +1,24 @@
 import { isRejectedWithValue } from '@reduxjs/toolkit'
 
-import type { MiddlewareAPI, Middleware } from '@reduxjs/toolkit'
+import type { Middleware } from '@reduxjs/toolkit'
 
-export const errorLoggerMiddleware: Middleware =
-  /* eslint-disable */ // 'api' is defined but never used  @typescript-eslint/no-unused-vars
-  (api: MiddlewareAPI) => (next) => (action) => {
-    if (isRejectedWithValue(action)) {
-      const statusCode = action.payload?.status
-      switch (statusCode) {
-        case 401: {
-          alert('Ошибка авторизации. Проверьте логин и пароль')
-          break
-        }
-        case 'FETCH_ERROR': {
-          alert('Не проходит запрос на сервер. Обратитесь в службу поддержки')
-          break
-        }
-        default: {
-          alert('Произошла ошибка. Попробуйте ещё раз')
-          break
-        }
+export const errorLoggerMiddleware: Middleware = () => (next) => (action) => {
+  if (isRejectedWithValue(action)) {
+    const statusCode = action.payload?.status
+    switch (statusCode) {
+      case 401: {
+        alert('Ошибка авторизации. Проверьте логин и пароль')
+        break
+      }
+      case 'FETCH_ERROR': {
+        alert('Не проходит запрос на сервер. Обратитесь в службу поддержки')
+        break
+      }
+      default: {
+        alert('Произошла ошибка. Попробуйте ещё раз')
+        break
       }
     }
-    return next(action)
   }
+  return next(action)
+}
